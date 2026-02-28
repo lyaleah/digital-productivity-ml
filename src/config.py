57d0f643a7097323e23f3c4cebaaeeb3a374bcac
lyaleah <leahluo3260@163.com>
2026-02-28 02:35:04 +0000

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,12 @@
+RANDOM_SEED = 42
+TEST_SIZE = 0.2
+TARGET = "productivity_score"
+
+N_JOBS = -1
+CV_FOLDS = 5
+
+DATA_PATH = "data/raw/student_productivity_distraction_dataset_20000.csv"
+
+MODEL_OUTPUT_PATH = "outputs/models/best_model.pkl"
+CV_RESULTS_PATH = "outputs/cv_results.csv"
+TEST_METRICS_PATH = "outputs/metrics/test_metrics.json"
